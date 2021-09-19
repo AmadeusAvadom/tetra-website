564c4eab0f33f68e325f1b744773e37b2035d877
RedCode26 <judaslinarez@gmail.com>
2021-09-19 10:51:24 -0500

    Body

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import { Banner } from '../components/Banner'
+import Footer from '../components/Footer'
+import Four from '../components/Four'
 import { One } from '../components/One'
+import Three from '../components/Three'
+import { Two } from '../components/Two'
 import "../styles/main.css"
 export const Home = () => {
     return (
         <div>
             <Banner/>
             <One/>
+            <Two/>
+            <Three/>
+            <Four/>
+            <Footer/>
         </div>
     )
 }
